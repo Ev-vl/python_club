f179e054050bd42da7fb20e09ab3710f12dd60fb
Vlad <mysholovskiyv@gmail.com>
2022-09-05 22:53:03 +0300

    Added if for how are you question

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-name = input("Your name")
+def input_name():
+    return input("Your name ")
 
-print(f"Hello, {name}")
+def how_are_you(name):
+    print(f"Are you OK, {name}?")
+    a = input()
+    if a in ('Yes', 'yes'):
+        print("That's cool!")
+    else:
+        print("Don't worry :)")
+
+name1 = input_name()
+print(f"Hello, {name1}")
+how_are_you(name1)
