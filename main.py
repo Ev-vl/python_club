46bd5313fb4bd2d0424f44abdc1c3a414df83817
Vlad <mysholovskiyv@gmail.com>
2022-09-05 23:00:52 +0300

    Added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#Function for input name
 def input_name():
     return input("Your name ")
 
+#Function for asking how are you (with name variable)
 def how_are_you(name):
     print(f"Are you OK, {name}?")
     if input() in ('Yes', 'yes'):
@@ -8,6 +10,7 @@ def how_are_you(name):
     else:
         print("Don't worry :)")
 
+#Main part
 name1 = input_name()
 print(f"Hello, {name1}")
 how_are_you(name1)
