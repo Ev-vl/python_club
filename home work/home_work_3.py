20eab52b434fd34d7f4a5bd45951f24438dceba4
Vlad <mysholovskiyv@gmail.com>
2022-10-20 23:40:27 +0300

    added home work

---
+++ home work/home_work_3.py
@@ -0,0 +1,9 @@
+
+str1 = """Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may.
+
+Frankness applauded by supported ye household. Collected favourite now for for and rapturous repulsive consulted. An seems green be wrote again. She add what own only like. Tolerably we as extremity exquisite do commanded. Doubtful offended do entrance of landlord moreover is mistress in. Nay was appear entire ladies. Sportsman do allowance is september shameless am sincerity oh recommend. Gate tell man day that who.
+"""
+
+str2 = str1.split(' ')
+
+print(str2)
